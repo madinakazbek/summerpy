7f528e9243aaaba6015afd86ddc68280d0b46aa7
madinakazbek <madkazbek@gmail.com>
2021-06-25 17:44:25 +0600

    w3schools upd

---
+++ week1/math.py
@@ -0,0 +1,11 @@
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+print(int(math.sqrt(64)))
+
+print(math.ceil(12.6), math.floor(89.3))
---
+++ week1/module_test_import.py
@@ -0,0 +1,3 @@
+import module_test
+
+module_test.greeting("Madina")
